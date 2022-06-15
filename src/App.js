42139df2ddb8d0bdbbae22d5c8da5a862d508fcd
nurselaine <Elaine.t.huynh@gmail.com>
2022-06-14 21:25:39 -0700

    Finished adding favorite feature and bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+// import { render } from '@testing-library/react';
+import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </>
+    );
+  }
 }
 
 export default App;
